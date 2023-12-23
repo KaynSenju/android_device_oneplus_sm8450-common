814cd8d43950e871edf81473a1e842f92a3678c9
Machad3x <machad3x@gmail.com>
2023-12-23 08:40:54 -0400

    sm8450-common: Import kernel init scripts from stock

--- common.mk
+++ common.mk
@@ -260,6 +260,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
     init.class_main.sh \
+    init.kernel.post_boot-taro.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
@@ -269,6 +270,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.qti.kernel.rc \
     init.target.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc
