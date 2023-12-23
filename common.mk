559bad963664e3768a875ed603b9d5907d0a017e
Machad3x <machad3x@gmail.com>
2023-12-23 07:41:40 -0400

    sm8450-common: Copy Cgroup and task_profiles from system/core

--- common.mk
+++ common.mk
@@ -117,6 +117,11 @@ PRODUCT_PACKAGES += \
 # Blur
 TARGET_ENABLE_BLUR := true
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.cameraservice.service@2.2.vendor \
